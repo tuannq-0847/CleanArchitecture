e97d1b7a6af6e3ece5ccb4decb32207c423f6984
tuannguyen <tuannguyen@tuannguyens-MacBook-Pro.local>
2021-04-24 15:28:04 +0700

    update gradle, update newest version

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,24 +1,24 @@
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
 
 plugins {
-    kotlin("jvm").version("1.3.72")
+    kotlin("jvm").version("1.4.31")
 }
 
 buildscript {
     val hiltVersion = "2.33-beta"
-    val kotlinVersion by extra("1.3.72")
+//    val kotlinVersion by extra("1.4.31")
     repositories {
         google()
         jcenter()
         mavenLocal()
     }
     dependencies {
-        classpath("com.android.tools.build:gradle:4.1.1")
-        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.3.61")
+        classpath("com.android.tools.build:gradle:4.2.0-rc01")
+        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.4.31")
         // NOTE: Do not place your application dependencies here; they belong
         // in the individual module build.gradle files
         classpath("com.jfrog.bintray.gradle:gradle-bintray-plugin:1.8.5")
-        classpath("org.jetbrains.dokka:dokka-gradle-plugin:0.10.1")
+        classpath("org.jetbrains.dokka:dokka-gradle-plugin:1.4.0-rc")
         classpath("com.google.dagger:hilt-android-gradle-plugin:$hiltVersion")
     }
 }
