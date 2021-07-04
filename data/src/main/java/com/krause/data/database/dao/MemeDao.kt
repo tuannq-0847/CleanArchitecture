fa5d269280b31e33db502f721f2296569d85d809
tuannguyen <tuannguyen@tuannguyens-MacBook-Pro.local>
2021-07-04 16:04:55 +0700

    Implement Fast Permissions

--- data/src/main/java/com/krause/data/database/dao/MemeDao.kt
+++ data/src/main/java/com/krause/data/database/dao/MemeDao.kt
@@ -3,9 +3,8 @@ package com.krause.data.database.dao
 import androidx.room.Dao
 import androidx.room.Insert
 import androidx.room.Query
-import com.krause.data.database.MEME_TABLE_NAME
+import com.krause.data.MEME_TABLE_NAME
 import com.krause.data.database.model.MemeEntity
-import com.krause.domain.model.Meme
 
 @Dao
 interface MemeDao {
